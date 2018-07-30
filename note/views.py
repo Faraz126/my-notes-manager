ef90ef085c95e11b91fa4aaab16e4f61f31a7a96
fk03983 <fk03983@st.habib.edu.pk>
2018-07-30 21:11:44 +0500

    added templates

--- note/views.py
+++ note/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Note
+from django.utils import timezone
 # Create your views here.
 
 def note_list(request):
-    return render(request, 'note/note_list.html', {})
+    notes = Note.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'note/note_list.html', {'notes': notes})
