620bde9d3d502982113b1c92d71eba2d3ccb6c20
fk03983 <fk03983@st.habib.edu.pk>
2018-07-31 15:01:33 +0500

    extended applications

--- note/urls.py
+++ note/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.note_list, name = 'note_list')
+    path('', views.note_list, name = 'note_list'),
+    path('note/<int:pk>/', views.note_detail, name='note_detail')
 ]
--- note/views.py
+++ note/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Note
 from django.utils import timezone
 # Create your views here.
@@ -6,3 +6,7 @@ from django.utils import timezone
 def note_list(request):
     notes = Note.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'note/note_list.html', {'notes': notes})
+
+def note_detail(request, pk):
+    note = Note.objects.get(pk=pk)
+    return render(request, 'note/note_detail.html', {'note':note})
