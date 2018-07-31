2601f53a9e9647b6525533f906d30f04a087c444
fk03983 <fk03983@st.habib.edu.pk>
2018-07-31 19:29:17 +0500

    added new/edit option

--- note/views.py
+++ note/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Note
+from .forms import NoteForm
 from django.utils import timezone
+from django.shortcuts import redirect
+
 # Create your views here.
 
 def note_list(request):
@@ -10,3 +13,32 @@ def note_list(request):
 def note_detail(request, pk):
     note = Note.objects.get(pk=pk)
     return render(request, 'note/note_detail.html', {'note':note})
+
+def note_new(request):
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.author = request.user
+            note.published_date = timezone.now()
+            note.save()
+            return redirect('note_detail', pk=note.pk)
+    else:
+        form = NoteForm()
+    return render(request, 'note/note_edit.html', {'form': form})
+
+def note_edit(request, pk):
+    note  =get_object_or_404(Note, pk=pk)
+    if request.method == "POST":
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.author = request.user
+            note.published_date = timezone.now()
+            note.save()
+            return redirect('note_detail', pk=note.pk)
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'note/note_edit.html', {'form': form})
+
+
